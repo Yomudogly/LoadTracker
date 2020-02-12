ddd810209f608d3f481beec51f0f7bc116e5dc80
Yomudogly <khalnepesov@gmail.com>
2020-02-11 19:57:03 -0500

    deleted validation for vin

--- application/routes.py
+++ application/routes.py
@@ -7,6 +7,7 @@ from application.forms import LoginForm
 from application.utils import encoder
 from flask_restplus import Resource, abort
 from flask_jwt_simple import jwt_required, create_jwt, get_jwt_identity
+from werkzeug.exceptions import BadRequest
 
 
 
@@ -249,7 +250,7 @@ class ActGetAndPost(Resource):
             return json.loads(resp_string), 200
             # return jsonify(Activity.objects(act_id=activity.act_id))
         else:
-            return jsonify(message="This vin does not exist"), 400
+            raise BadRequest(message="This vin does not exist")
         # ???  how to specify input of DateTimeField ????
         # "start_time": "2020-01-1 20:09:44"
           
