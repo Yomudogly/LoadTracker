d6d5599ad8ed127257234815bf583f085add4ed4
Yomudogly <khalnepesov@gmail.com>
2020-02-12 21:16:46 -0500

    changed time to now

--- application/models.py
+++ application/models.py
@@ -1,7 +1,13 @@
 import flask
-from datetime import datetime
 from application import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime
+from pytz import timezone
+from tzlocal import get_localzone
+
+# Current time in UTC
+now_utc = datetime.now(timezone('UTC'))
+now_local = now_utc.astimezone(get_localzone())
 
 
 class Fullfilment_center(db.Document):
@@ -32,11 +38,11 @@ class Van(db.Document):
 class Schedule_wave(db.Document):
     wave_id = db.IntField( unique=True )
     company_id = db.IntField()
-    start_time = db.DateTimeField( default=datetime.now() )
-    end_time = db.DateTimeField( default=datetime.now() )
+    start_time = db.DateTimeField( default=now_local )
+    end_time = db.DateTimeField( default=now_local )
     status = db.BooleanField( default=True )
     
 class Activity(db.Document):
     act_id = db.IntField( unique=True )
     vin = db.StringField()
-    scan_time = db.DateTimeField( default=datetime.now() )
+    scan_time = db.DateTimeField( default=now_local )
